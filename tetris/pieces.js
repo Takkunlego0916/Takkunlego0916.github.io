cf60dfc9430ed2758719f8caf6850d77e101278b
TakkunMC_jp <Kaneda.family.803@gmail.com>
2025-12-07 07:56:46 +0900

    Update pieces.js

--- tetris/pieces.js
+++ tetris/pieces.js
@@ -1,11 +1,3 @@
-const TETROMINOES={
- I:[[1,1,1,1]],
- O:[[1,1],[1,1]],
- T:[[0,1,0],[1,1,1]],
- S:[[0,1,1],[1,1,0]],
- Z:[[1,1,0],[0,1,1]],
- J:[[1,0,0],[1,1,1]],
- L:[[0,0,1],[1,1,1]]
-};
-const COLORS={I:'#0ff',O:'#ff0',T:'#a0f',S:'#0f0',Z:'#f55',J:'#08f',L:'#f80'};
-const KEYS={37:'left',39:'right',40:'down',38:'rotate',32:'drop',80:'pause'};
+const TETROMINOES={I:[[1,1,1,1]],O:[[1,1],[1,1]],T:[[0,1,0],[1,1,1]],S:[[0,1,1],[1,1,0]],Z:[[1,1,0],[0,1,1]],J:[[1,0,0],[1,1,1]],L:[[0,0,1],[1,1,1]]};
+const COLORS={I:'#00e5ff',O:'#ffd600',T:'#b388ff',S:'#00e676',Z:'#ff5252',J:'#2979ff',L:'#ff9100'};
+const KEYS={ArrowLeft:'left',ArrowRight:'right',ArrowDown:'down',ArrowUp:'rotate', ' ': 'drop'};
