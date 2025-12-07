5aad7330b1324687c5f029d519a87742f4fec8d2
TakkunMC_jp <Kaneda.family.803@gmail.com>
2025-12-07 09:19:24 +0900

    Delete tetris directory

--- tetris/pieces.js
+++
@@ -1,3 +0,0 @@
-const TETROMINOES={I:[[1,1,1,1]],O:[[1,1],[1,1]],T:[[0,1,0],[1,1,1]],S:[[0,1,1],[1,1,0]],Z:[[1,1,0],[0,1,1]],J:[[1,0,0],[1,1,1]],L:[[0,0,1],[1,1,1]]};
-const COLORS={I:'#00e5ff',O:'#ffd600',T:'#b388ff',S:'#00e676',Z:'#ff5252',J:'#2979ff',L:'#ff9100'};
-const KEYS={ArrowLeft:'left',ArrowRight:'right',ArrowDown:'down',ArrowUp:'rotate', ' ': 'drop'};
